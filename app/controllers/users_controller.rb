e073ddae37ac678f5cfd10481a2fc4e3bc502b47
Emma <seoyoon7011@gmail.com>
2018-02-06 14:23:59 +1100

    sign up and log in page done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.new
+    user.name = params[:name]
+    user.email = params[:email]
+    user.password = params[:password]
+    if user.save
+      session[:user_id] = user.id
+      redirect_to "/users/#{user.id}"
+    else
+      render '/users/new'
+    end
+  end
+
+  def show
+   @user = User.find(params[:id])
+  end
 end
