3d9342861beaf19399f49318b718d3f4f29b86b8
Haofu <haofu.zhu@gmail.com>
2018-02-06 13:02:43 +1100

    Add start_time and user_id to teams table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206001209) do
+ActiveRecord::Schema.define(version: 20180206020212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180206001209) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img_url"
   end
 
   create_table "species", force: :cascade do |t|
@@ -86,6 +87,8 @@ ActiveRecord::Schema.define(version: 20180206001209) do
     t.integer "member_6"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "start_time"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
