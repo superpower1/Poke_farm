a6bf8aea42ddfd64d42dc7ea4764c1ba8ab47e98
Emma <seoyoon7011@gmail.com>
2018-02-08 10:44:42 +1100

    species index page done

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,15 +1,22 @@
 class SpeciesController < ApplicationController
 	def index
-		@species = Species.all
+		 # @species = Species.all
+		 @species = Species.paginate(:page => params[:page], :per_page => 10)
 	end
 
-	def show
-		@specie = Species.find_by(identifier:params[:identifier])
-		@identify_species = Species.where("identifier LIKE ?", "%#{params[:identifier]}%")
-		if params[:fuzzy_search]
-			render :json => @identify_species
-			return
-		end
-		render :json => @specie
+	# def show
+	# 	@identify_species = Species.where("identifier LIKE ?", "%#{params[:identifier]}%")
+	# 	if params[:fuzzy_search]
+	# 		render :json => @identify_species
+	# 		return
+	# 	end
+	# 	render :json => @specie
+	# end
+
+	def api_index
+		 @specie = Species.find_by(identifier: params[:identifier])
+		 @species_chain_id = @specie.evolution_chain_id
+		 @species = Species.where(evolution_chain_id: @species_chain_id)
+		 render json: @species
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :species, only: [:index,:show]
   resources :habitats, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/api_index', to: 'species#api_index'
   post '/sessions', to: 'sessions#create'
   delete '/sessions', to: 'sessions#destroy'
   root 'pages#home'
