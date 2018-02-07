db5faf053bf15e61a85f3608386e8a635ed803df
Haofu <haofu.zhu@gmail.com>
2018-02-07 21:55:01 +1100

    modify team page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -20,6 +20,7 @@ class TeamsController < ApplicationController
 	def create
 		new_team = Team.new
 		new_team.user_id = session[:user_id]
+		new_team.name = params[:name]
 		new_team.save
 		redirect_to "/teams"
 	end
