6450087260986ee5cc2886cf5d10d6077088364f
Emma <seoyoon7011@gmail.com>
2018-02-08 15:03:09 +1100

    shop table records

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,13 +2,15 @@ class ShopsController < ApplicationController
 
 	def index
 		@shops = Shop.all
-		@pokemons = MyPokemon.where(user_id: '1')	
-		@user = User.find('1')
+		@pokemons = MyPokemon.where(user_id: session[:user_id])
+		@user = User.find(session[:user_id])
 		@mypokemon = MyPokemon.all
+
+
 	end
 
 	def create
-		@user = User.find('1') # change this to session  [:user_id]
+		@user = User.find(session[:user_id]) # change this to session  [:user_id]
 		@mymoney = @user.money
 		@actionType = params[:actionType]
 
@@ -16,9 +18,9 @@ class ShopsController < ApplicationController
 		if @actionType == "buy"
 		@totalPrice = params[:totalPrice]
 		@mymoney = @mymoney - @totalPrice.to_i
-		
+
 		elsif @actionType == "sell"
-			
+
 		#if actionType is sell
 		@totalIncome = params[:totalIncome]
 		@mymoney = @mymoney + @totalIncome.to_i
--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -4,19 +4,31 @@ class SpeciesController < ApplicationController
 		 @species = Species.paginate(:page => params[:page], :per_page => 10)
 	end
 
-	# def show
-	# 	@identify_species = Species.where("identifier LIKE ?", "%#{params[:identifier]}%")
-	# 	if params[:fuzzy_search]
-	# 		render :json => @identify_species
-	# 		return
-	# 	end
-	# 	render :json => @specie
-	# end
+	def show
+		@identify_species = Species.where("identifier LIKE ?", "%#{params[:identifier]}%")
+		if params[:fuzzy_search]
+			render :json => @identify_species
+			return
+		end
+		render :json => @specie
+	end
 
 	def api_index
 		 @specie = Species.find_by(identifier: params[:identifier])
 		 @species_chain_id = @specie.evolution_chain_id
 		 @species = Species.where(evolution_chain_id: @species_chain_id)
-		 render json: @species
+		 # @specie_habitat = Species_habitats_relationship.find_by(species_id: @specie.id)
+     # @habitat = Habitat.find(@specie_habitat.habitat_id)
+		 # list = []
+		 # @species.each do |s|
+			#  list << {
+			# 	 color: s.color,
+			# 	 habitat: s.habitat
+			#  }
+		 # end
+
+		 # render json: list
+		 render json: @species, methods: [:habitat]
+		 # render json: @habitat
 	end
 end
